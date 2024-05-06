5e7b49924341c78022b9dc6514670f6d682041d1
l3x-fx <kekskiller@email.com>
2024-05-06 20:00:09 +0200

    initial commit

---
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -0,0 +1,72 @@
+package com.example.demo.controllers;
+
+import com.example.demo.model.persistence.Cart;
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.persistence.repositories.CartRepository;
+import com.example.demo.model.persistence.repositories.UserRepository;
+import com.example.demo.model.requests.CreateUserRequest;
+import com.example.demo.model.requests.LoginUserRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+	private static final Logger log = LoggerFactory.getLogger(UserController.class);
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private CartRepository cartRepository;
+
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	@GetMapping("/id/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		return ResponseEntity.of(userRepository.findById(id));
+	}
+
+	@GetMapping("/{username}")
+	public ResponseEntity<User> findByUserName(@PathVariable String username) {
+		User user = userRepository.findByUsername(username);
+		return user == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(user);
+	}
+	@PostMapping("/login")
+	public ResponseEntity<User> loginUser(@RequestBody LoginUserRequest loginUserRequest) {
+
+        return null;
+    }
+
+
+	@PostMapping("/create")
+	public ResponseEntity<User> createUser(@RequestBody CreateUserRequest createUserRequest) {
+		User user = new User();
+		user.setUsername(createUserRequest.getUsername());
+
+		log.info("Username set to {}", createUserRequest.getUsername());
+		Cart cart = new Cart();
+
+		cartRepository.save(cart);
+		user.setCart(cart);
+		if(createUserRequest.getPassword().length()<7 ||
+				!createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword())){
+		return ResponseEntity.badRequest().build();
+		}
+		user.setPassword(bCryptPasswordEncoder.encode(createUserRequest.getPassword()));
+		userRepository.save(user);
+		return ResponseEntity.ok(user);
+	}
+
+}
---
+++ src/test/java/com/example/demo/controllers/UserControllerTest.java
@@ -0,0 +1,54 @@
+package com.example.demo.controllers;
+
+import com.example.demo.TestUtils;
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.persistence.repositories.CartRepository;
+import com.example.demo.model.persistence.repositories.UserRepository;
+import com.example.demo.model.requests.CreateUserRequest;
+import org.h2.command.ddl.CreateUser;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class UserControllerTest {
+
+    private UserController userController;
+    private UserRepository userRepository = mock(UserRepository.class);
+    private CartRepository cartRepository = mock(CartRepository.class);
+    private BCryptPasswordEncoder encoder = mock(BCryptPasswordEncoder.class);
+
+    @Before
+    public void setUp(){
+        userController = new UserController();
+        TestUtils.injectObjects(userController, "userRepository", userRepository);
+        TestUtils.injectObjects(userController, "cartRepository", cartRepository);
+        TestUtils.injectObjects(userController, "bCryptPasswordEncoder", encoder);
+    }
+
+    @Test
+    public void createUser_happyPath() throws Exception{
+        when(encoder.encode("testPassword")).thenReturn("thisIsHashed");
+        CreateUserRequest req = new CreateUserRequest();
+        req.setUsername("test");
+        req.setPassword("testPassword");
+        req.setConfirmPassword("testPassword");
+
+        final ResponseEntity<User> response = userController.createUser(req);
+
+        assertNotNull(response);
+        assertEquals(200, response.getStatusCodeValue());
+
+        User user = response.getBody();
+        assertNotNull(user);
+        assertEquals(0, user.getId());
+        assertEquals("test", user.getUsername());
+        assertEquals("thisIsHashed", user.getPassword());
+    }
+
+}
